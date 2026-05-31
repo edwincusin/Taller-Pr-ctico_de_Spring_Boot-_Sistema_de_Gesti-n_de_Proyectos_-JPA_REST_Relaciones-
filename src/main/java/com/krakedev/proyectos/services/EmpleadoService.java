4a559083a0a588433f88d433eb6f540f508b3ce4
EDWINCSN <c.edwin32@hotmail.com>
2026-05-31 01:06:40 -0500

    feat(EGCA): crear servicios de empleado

---
+++ src/main/java/com/krakedev/proyectos/services/EmpleadoService.java
@@ -0,0 +1,55 @@
+package com.krakedev.proyectos.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.krakedev.proyectos.entidades.Empleado;
+import com.krakedev.proyectos.entidades.Proyecto;
+import com.krakedev.proyectos.repositories.EmpleadoRepository;
+
+@ Service
+public class EmpleadoService {
+	
+	private final EmpleadoRepository repositoryEmpleado;
+
+
+	public EmpleadoService(EmpleadoRepository repositoryEmpleado) {
+		super();
+		this.repositoryEmpleado = repositoryEmpleado;
+	}
+
+	//INSERTAR NUEVO
+	public Empleado insertar(Empleado empleado) {
+		return repositoryEmpleado.save(empleado);
+	}
+	
+	// BUSCAR POR ID
+	public Empleado buscarPorID(Long id) {
+		Empleado empleadoEnocntrado = repositoryEmpleado.findById(id)
+				.orElseThrow(()-> new RuntimeException("No existe empleado con la ID: " + id));
+		return empleadoEnocntrado;
+	}
+
+	// LISTAR TODOS
+	public List<Empleado> listar() {
+		return repositoryEmpleado.findAll();
+	}
+	
+	//ACTUALIZAR 
+	public Empleado actualizar(Long id, Proyecto empleadoNuevo) {
+		Empleado empleado=buscarPorID(id);
+		
+		empleado.setNombre(empleadoNuevo.getNombre());
+		empleado.setCargo(empleado.getCargo());
+		
+		return repositoryEmpleado.save(empleado);
+	}
+	
+	//ELIMINAR
+	public void eliminar(Long id) {
+	    Empleado empleado= buscarPorID(id);
+	    repositoryEmpleado.delete(empleado);
+	}
+
+}
