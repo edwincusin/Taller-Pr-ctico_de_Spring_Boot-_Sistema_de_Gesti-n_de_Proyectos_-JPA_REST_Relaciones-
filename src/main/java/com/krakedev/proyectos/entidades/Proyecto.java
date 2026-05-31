3a8672b8a085479b2ba0933868faa51333a9123a
EDWINCSN <c.edwin32@hotmail.com>
2026-05-30 23:05:58 -0500

    feat(EGCA): crear Clase Proyecto con anotaciones de @Entity

---
+++ src/main/java/com/krakedev/proyectos/entidades/Proyecto.java
@@ -0,0 +1,100 @@
+package com.krakedev.proyectos.entidades;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="proyectos")
+public class Proyecto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 100)
+	private String nombre;
+	
+	@Column(length = 250)
+	private String descripcion;
+	
+	@Column(nullable = false)
+	private LocalDate fechaInicio;
+	
+	@JsonIgnore // para evitar una bucle infinito
+	@OneToMany(mappedBy = "proyecto")
+	private List<Tarea> tareas;
+
+	
+	public Proyecto() {
+
+	}
+	
+	public Proyecto(String nombre, String descripcion, LocalDate fechaInicio) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public List<Tarea> getTareas() {
+		return tareas;
+	}
+
+	public void setTareas(List<Tarea> tareas) {
+		this.tareas = tareas;
+	}
+
+	@Override
+	public String toString() {
+		return "Proyecto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fechaInicio="
+				+ fechaInicio + ", tareas=" + tareas + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
