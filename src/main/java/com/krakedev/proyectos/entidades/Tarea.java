ee439a14decdee4f41031c6b47de3b15f8e1f056
EDWINCSN <c.edwin32@hotmail.com>
2026-05-30 23:36:22 -0500

    feat(EGCA): crear Clase tareas con anotaciones de @Entity

--- src/main/java/com/krakedev/proyectos/entidades/Tarea.java
+++ src/main/java/com/krakedev/proyectos/entidades/Tarea.java
@@ -1,5 +1,114 @@
 package com.krakedev.proyectos.entidades;
 
-public class Tarea {
+import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tareas")
+public class Tarea {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 200)
+	private String descripcion;
+	
+	@Column(nullable = false)
+	private LocalDate fechaLimite;
+	
+	@Column(nullable = false)
+	private double costoEstimado;
+	
+	@ManyToOne
+	@JoinColumn(name="proyecto_id", nullable = false)
+	private Proyecto proyecto;
+	
+	@ManyToMany
+	@JoinTable(
+				name="tarea_empleados",
+				joinColumns = @JoinColumn(name="tarea_id"), // clave foaena de la actual tabla 
+				inverseJoinColumns = @JoinColumn(name="empleado_id") // calve foreanea de la entidad relacionada
+			)
+	private List<Empleado> empleados;
+
+	public Tarea() {
+		super();
+	}
+
+	public Tarea(String descripcion, LocalDate fechaLimite, double costoEstimado, Proyecto proyecto,
+			List<Empleado> empleados) {
+		super();
+		this.descripcion = descripcion;
+		this.fechaLimite = fechaLimite;
+		this.costoEstimado = costoEstimado;
+		this.proyecto = proyecto;
+		this.empleados = empleados;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LocalDate getFechaLimite() {
+		return fechaLimite;
+	}
+
+	public void setFechaLimite(LocalDate fechaLimite) {
+		this.fechaLimite = fechaLimite;
+	}
+
+	public double getCostoEstimado() {
+		return costoEstimado;
+	}
+
+	public void setCostoEstimado(double costoEstimado) {
+		this.costoEstimado = costoEstimado;
+	}
+
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+	public List<Empleado> getEmpleados() {
+		return empleados;
+	}
+
+	public void setEmpleados(List<Empleado> empleados) {
+		this.empleados = empleados;
+	}
+
+	@Override
+	public String toString() {
+		return "Tarea [id=" + id + ", descripcion=" + descripcion + ", fechaLimite=" + fechaLimite + ", costoEstimado="
+				+ costoEstimado + ", proyecto=" + proyecto + ", empleados=" + empleados + "]";
+	}
+	
+	
 }
