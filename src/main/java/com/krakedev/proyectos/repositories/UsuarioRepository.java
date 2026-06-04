52125821d5fda295d8c05d34ba887c5091912514
EDWINCSN <c.edwin32@hotmail.com>
2026-06-04 00:05:52 -0500

    refactor(EGCA): modificar interfas UsuarioRepository con herencia de JPARepository

--- src/main/java/com/krakedev/proyectos/repositories/UsuarioRepository.java
+++ src/main/java/com/krakedev/proyectos/repositories/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package com.krakedev.proyectos.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.krakedev.proyectos.entidades.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
+	Optional<Usuario> findByUsername(String username);
 }
