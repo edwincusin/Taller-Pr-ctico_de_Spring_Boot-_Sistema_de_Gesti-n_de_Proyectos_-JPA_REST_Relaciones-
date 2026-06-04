d497a72d1eaee80348797cf3ae9bb4c3deed5a53
EDWINCSN <c.edwin32@hotmail.com>
2026-06-04 00:04:19 -0500

    add(EGCA): agrega interfas UsuarioRepository con herencia de JPARepository

---
+++ src/main/java/com/krakedev/proyectos/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.krakedev.proyectos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.krakedev.proyectos.entidades.Usuario;
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
